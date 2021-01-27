22dde07948133108d45106f1f5972462edf8f29f
HerrMenne <herr.menne@gmail.com>
2021-01-27 10:01:09 +0100

    Button vergrößert

--- ITFgit/ITFgit/Form1.Designer.cs
+++ ITFgit/ITFgit/Form1.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(355, 189);
+            this.button1.Location = new System.Drawing.Point(237, 189);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(193, 127);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
